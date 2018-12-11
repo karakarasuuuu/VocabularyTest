31a63daf329f147788312ff0638134a91fc59035
karasu <GTR12534@Gmail.com>
2018-12-11 22:30:22 +0800

    Count for solved problems, print the correct answer when failing.
    Now it can count how many problems you have solved, and will tell you the correct answer if you fail.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@ using namespace std;
 int main(){
     string input;
     srand(time(NULL));
-    int index;
+    int index, nosp; // nosp stands for # Of Solved Problems
     while(1){
+        nosp = 0;
         cout << "Enter 1 to start the game, enter 0 to end the program: ";
         cin >> input;
         if (input == "0") // end the program
@@ -17,14 +18,14 @@ int main(){
         else if (input == "1"){ // start the game
             while(1){
                 index = (rand() % (sizeOfQuestion / 2)) * 2;
-                cout << question[index] << " → "; 
+                cout << question[index] << " > "; 
                 cin >> input;
                 if (input == question[index + 1]){
-                    cout << "Correct answer." << endl;
+                    cout << "Correct answer. \nYou have solved " << ++nosp << ((nosp == 1)? " problem.": " problems.") << endl;
                     continue;
                 }
                 else {
-                    cout << "Wrong answer." << endl;
+                    cout << "Wrong answer. \nThe correct answer is "<< question[index + 1] << ".\nYou solved " << nosp << " problems this time." << endl;
                     break;
                 }
             }
